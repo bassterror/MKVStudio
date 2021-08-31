a030c64025eea8872e0996f797a604963428e4ed
Miroslav Petrov <miroslav_petrov@outlook.com>
2021-08-31 14:37:50 +0300

    Implement Dependency Injection system

--- MKVStudio/App.xaml.cs
+++ MKVStudio/App.xaml.cs
@@ -1,4 +1,10 @@
-﻿using System.Windows;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MKVStudio.Handlers;
+using MKVStudio.ViewModels;
+using System;
+using System.IO;
+using System.Windows;
 
 namespace MKVStudio
 {
@@ -9,10 +15,30 @@ namespace MKVStudio
     {
         protected override void OnStartup(StartupEventArgs e)
         {
-            Window window = new MainWindow();
-            window.Show();
+            IServiceProvider serviceProvider = CreateServiceProvider();
+
+            MainWindow mainWindow = serviceProvider.GetRequiredService<MainWindow>();
+            mainWindow.Show();
 
             base.OnStartup(e);
+        }
+
+        private IServiceProvider CreateServiceProvider()
+        {
+            IConfigurationBuilder builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+            IConfiguration configuration = builder.Build();
+
+            IServiceCollection services = new ServiceCollection();
+
+            _ = services.Configure<AppSettings>(configuration.GetSection(nameof(AppSettings)));
+
+            _ = services.AddScoped<MainWindowViewModel>();
+            _ = services.AddScoped(f => new MainWindow(f.GetRequiredService<MainWindowViewModel>()));
+
+            return services.BuildServiceProvider();
         }
     }
 }
