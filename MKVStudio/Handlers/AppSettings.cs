c5a01f6820c4c8673fdb6d1a6d935632b9c3c6e7
Miroslav Petrov <miroslav_petrov@outlook.com>
2021-08-31 14:36:45 +0300

    Implement appsettings.json

---
+++ MKVStudio/Handlers/AppSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace MKVStudio.Handlers
+{
+    public class AppSettings
+    {
+        public string FfmpegPath { get; set; }
+        public string MkvInfoPath { get; set; }
+        public string MkvMergePath { get; set; }
+        public string MkvPropEditPath { get; set; }
+        public string MkvExtractPath { get; set; }
+    }
+}
