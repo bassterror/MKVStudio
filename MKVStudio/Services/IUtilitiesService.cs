5d7523722bdba896c9082508457beae188acdc8b
Miroslav Petrov <miroslav_petrov@outlook.com>
2021-12-19 17:50:33 +0200

    [#20] Code cleanup
    More detailed explanatory text. Wrap it to 72 characters. The blank
    line separating the summary from the body is critical (unless you omit
    the body entirely).
    
    Write your commit message in the imperative: "Fix bug" and not "Fixed
    bug" or "Fixes bug." This convention matches up with commit messages
    generated by commands like git merge and git revert.
    
    Further paragraphs come after blank lines.
    
    - Bullet points are okay, too.
    - Typically a hyphen or asterisk is used for the bullet, followed by a
      single space. Use a hanging indent.

--- MKVStudio/Services/IUtilitiesService.cs
+++ MKVStudio/Services/IUtilitiesService.cs
@@ -7,10 +7,6 @@ namespace MKVStudio.Services
     {
         string GetExecutable(Executables executable);
         OpenFileDialog GetFileDialog(string filter, bool multiselect);
-        /// <summary>
-        /// Gets files from selected folder
-        /// </summary>
-        /// <returns>Array of file paths</returns>
         string[] GetFilesFromFolder(string complexFilter);
         string GetFolder();
         string ConvertBytes(long value, int decimalPlaces = 1);
