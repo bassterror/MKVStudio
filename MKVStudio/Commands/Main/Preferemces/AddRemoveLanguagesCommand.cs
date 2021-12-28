e95a20045a621b01d95d0bbb88241c3dc9b366d8
Miroslav Petrov <miroslav_petrov@outlook.com>
2021-12-28 12:11:07 +0200

    [#23] Create command for adding and removing preferred languages
    More detailed explanatory text. Wrap it to 72 characters. The blank
    line separating the summary from the body is critical (unless you omit
    the body entirely).
    
    Write your commit message in the imperative: "Fix bug" and not "Fixed
    bug" or "Fixes bug." This convention matches up with commit messages
    generated by commands like git merge and git revert.
    
    Further paragraphs come after blank lines.
    
    - Bullet points are okay, too.
    - Typically a hyphen or asterisk is used for the bullet, followed by a
      single space. Use a hanging indent.

---
+++ MKVStudio/Commands/Main/Preferemces/AddRemoveLanguagesCommand.cs
@@ -0,0 +1,66 @@
+﻿using MKVStudio.Models;
+using MKVStudio.ViewModels;
+using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows.Input;
+
+namespace MKVStudio.Commands
+{
+    public class AddRemoveLanguagesCommand : ICommand
+    {
+        private readonly PreferencesVM _preferences;
+
+        public event EventHandler CanExecuteChanged { add { } remove { } }
+
+        public AddRemoveLanguagesCommand(PreferencesVM preferences)
+        {
+            _preferences = preferences;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            if (parameter is string value)
+            {
+                switch (value)
+                {
+                    case "add":
+                        _preferences.SearchAvailableLanguage = string.Empty;
+                        _preferences.ExLib.Languages.Add(_preferences.SelectedAvailableLanguage);
+                        _preferences.ExLib.Languages = new ObservableCollection<Language>(_preferences.ExLib.Languages.OrderBy(l => l.Name));
+                        _preferences.ExLib.AllLanguages.Remove(_preferences.SelectedAvailableLanguage);
+                        _preferences.ExLib.AllLanguages = new ObservableCollection<Language>(_preferences.ExLib.AllLanguages.OrderBy(l => l.Name));
+                        _preferences.UserLanguages = _preferences.ExLib.Languages;
+                        _preferences.AvailableLanguages = _preferences.ExLib.AllLanguages;
+                        _preferences.ExLib.Util.SetPreferedLanguages(GetPrefLangString());
+                        break;
+                    case "remove":
+                        _preferences.SearchUserLanguage = string.Empty;
+                        _preferences.ExLib.AllLanguages.Add(_preferences.SelectedUserLanguage);
+                        _preferences.ExLib.AllLanguages = new ObservableCollection<Language>(_preferences.ExLib.AllLanguages.OrderBy(l => l.Name));
+                        _preferences.ExLib.Languages.Remove(_preferences.SelectedUserLanguage);
+                        _preferences.ExLib.Languages = new ObservableCollection<Language>(_preferences.ExLib.Languages.OrderBy(l => l.Name));
+                        _preferences.AvailableLanguages = _preferences.ExLib.AllLanguages;
+                        _preferences.UserLanguages = _preferences.ExLib.Languages;
+                        _preferences.ExLib.Util.SetPreferedLanguages(GetPrefLangString());
+                        break;
+                }
+            }
+        }
+
+        private string GetPrefLangString()
+        {
+            string value = string.Empty;
+            foreach (Language language in _preferences.ExLib.Languages)
+            {
+                value += $"{language.ID}|";
+            }
+            return value.Remove(value.Length - 1, 1);
+        }
+    }
+}
