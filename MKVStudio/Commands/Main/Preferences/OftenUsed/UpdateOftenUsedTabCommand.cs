ce1174aa5536484a9bbb6c81b5ede5582afcaae5
Miroslav Petrov <miroslav_petrov@outlook.com>
2022-02-22 20:51:38 +0200

    [#25] Add navigation to [OftenUsed]

---
+++ MKVStudio/Commands/Main/Preferences/OftenUsed/UpdateOftenUsedTabCommand.cs
@@ -0,0 +1,33 @@
+﻿using MKVStudio.ViewModels;
+
+namespace MKVStudio.Commands;
+
+public class UpdateOftenUsedTabCommand : BaseCommand
+{
+    private readonly OftenUsedVM _oftenUsed;
+    private readonly PreferedLanguagesVM _prefLang;
+
+    public UpdateOftenUsedTabCommand(OftenUsedVM oftenUsed, PreferedLanguagesVM prefLang)
+    {
+        _oftenUsed = oftenUsed;
+        _prefLang = prefLang;
+    }
+
+    public override void Execute(object parameter)
+    {
+        if (parameter is ViewModelTypes viewModel)
+        {
+            SelectOftenUsedTab(viewModel);
+        }
+    }
+
+    private void SelectOftenUsedTab(ViewModelTypes viewModel)
+    {
+        switch (viewModel)
+        {
+            case ViewModelTypes.PreferedLanguages:
+                _oftenUsed.SelectedOftenUsedTab = _prefLang;
+                break;
+        }
+    }
+}
---
+++ MKVStudio/ViewModels/Main/Preferences/OftenUsed/OftenUsedVM.cs
@@ -0,0 +1,21 @@
+﻿using MKVStudio.Commands;
+using MKVStudio.Services;
+using System.Windows.Input;
+
+namespace MKVStudio.ViewModels;
+
+public class OftenUsedVM : BaseViewModel
+{
+    public IUtilitiesService Util { get; }
+    public BaseViewModel SelectedOftenUsedTab { get; set; }
+    public PreferedLanguagesVM PreferedLanguages { get; set; }
+
+    public ICommand UpdateOftenUsedTab => new UpdateOftenUsedTabCommand(this, PreferedLanguages);
+
+    public OftenUsedVM(IUtilitiesService util)
+    {
+        Util = util;
+        PreferedLanguages = new(util);
+        UpdateOftenUsedTab.Execute(ViewModelTypes.PreferedLanguages);
+    }
+}
